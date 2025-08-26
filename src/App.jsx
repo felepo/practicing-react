801a41857fef374a7100b67ecc21c70232361968
felepo <fecrackle@gmail.com>
2025-08-25 22:40:06 -0600

    Integrate TailwindCSS into the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Counter from './components/Counter'
 import ToggleButton from './components/ToggleButton'
 import NameForm from './components/NameForm'
 import CounterWithEffect from './components/CounterWithEffect'
-import './App.css'
 
 function App() {
 
   return (
     <>
+      <p className="text-3xl font-bold underline">Testing TailwindCSS</p>
       <h3><i>Counter</i></h3>
       <Counter />
       <br />
