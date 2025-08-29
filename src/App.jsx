7d3a7c59449f55d3e4689be8e6b6300f6245639f
felepo <fecrackle@gmail.com>
2025-08-28 22:49:12 -0600

    Add ThemeProvider and ThemeButton components to manage and toggle application theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { createContext, useContext, useState } from 'react';
 import Counter from './components/Counter'
 import ToggleButton from './components/ToggleButton'
 import NameForm from './components/NameForm'
@@ -8,27 +9,66 @@ import UserListWithLoading from './components/UserListWithLoading/UserListWithLo
 import SearchPosts from './components/SearchPosts/SearchPosts'
 import Counter2 from './components/Counter2/Counter2'
 
+const ThemeContext = createContext();
+ 
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      { children }
+    </ThemeContext.Provider>
+  )
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <button 
+      onClick={toggleTheme} 
+      style={{ 
+        backgroundColor: theme === 'light' ? '#fff' : '#333', 
+        color: theme === 'light' ? '#000' : '#fff',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer'
+      }}
+    >
+      Switch to {theme === 'light' ? 'dark' : 'light'} mode
+    </button>
+  )
+}
+
 function App() {
 
   return (
-    <>
-      {/* <p className="text-3xl font-bold underline">Testing TailwindCSS</p>
-      <h3><i>Counter</i></h3>
-      <Counter />
-      <br />
-      <h3><i>Toggle Button</i></h3>
-      <ToggleButton />
-      <br />
-      <h3><i>Name Form</i></h3>
-      <NameForm />
-      <h3><i>Counter with effect</i></h3>
-      <CounterWithEffect />
-      <UserList />
-      <br />
-      <UserListWithLoading />
-      <SearchPosts /> */}
-      <Counter2 />
-    </>
+    <ThemeProvider>
+      <ThemeButton />
+    </ThemeProvider>
+    // <>
+    //   <p className="text-3xl font-bold underline">Testing TailwindCSS</p>
+    //   <h3><i>Counter</i></h3>
+    //   <Counter />
+    //   <br />
+    //   <h3><i>Toggle Button</i></h3>
+    //   <ToggleButton />
+    //   <br />
+    //   <h3><i>Name Form</i></h3>
+    //   <NameForm />
+    //   <h3><i>Counter with effect</i></h3>
+    //   <CounterWithEffect />
+    //   <UserList />
+    //   <br />
+    //   <UserListWithLoading />
+    //   <SearchPosts />
+    //   <Counter2 />
+    // </>
   )
 }
 
